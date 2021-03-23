e786a3a9db49d27d05f87b30e6bf1c22cc300a7e
sandeep_tom <sandeepnyc600@gmail.com>
2021-03-22 21:10:23 -0500

    Initial commit

---
+++ addition.py
@@ -0,0 +1,6 @@
+def addition(a, b):
+    return a+b
+
+
+res = addition(12, 24)
+print(res)
---
+++ subtract.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def subtract(a, b):
+    return a-b
+
+
+arr = np.array([1, 2, 3, 35])
+print(arr)
+
+res = subtract(12, 3)
+print(res)
